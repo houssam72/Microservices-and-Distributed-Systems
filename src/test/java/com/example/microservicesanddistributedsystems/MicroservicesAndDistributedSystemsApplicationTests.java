ade73985dff46e1b4463f1476a8413ca55cb416c
houssam72 <brothersmarket72@gmail.com>
2024-12-04 23:31:01 +0100

    feat : mvn config

---
+++ src/test/java/com/example/microservicesanddistributedsystems/MicroservicesAndDistributedSystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicesanddistributedsystems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesAndDistributedSystemsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
