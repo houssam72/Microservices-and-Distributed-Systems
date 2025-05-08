5916f49f6d9cbe4d2c5a1ddf0c97c548cec13216
houssam72 <brothersmarket72@gmail.com>
2025-05-08 22:43:28 +0200

    distributed tracing using micrometer zipkin

--- Fraud/src/main/java/com/fraud/FraudController.java
+++ Fraud/src/main/java/com/fraud/FraudController.java
@@ -2,8 +2,10 @@ package com.fraud;
 
 import com.clients.fraud.FraudCheckResponse;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
@@ -15,7 +17,7 @@ public class FraudController {
     public FraudCheckResponse isFraudster(
             @PathVariable("customerId") Integer customerId
     ){
-
+        log.info("New fraud... {}", customerId);
         boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
 
         return new FraudCheckResponse(isFraudulentCustomer);
