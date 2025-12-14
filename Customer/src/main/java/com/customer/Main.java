eabf8691e1a7ff2850c52d5a8db4b43489541272
houssam72 <brothersmarket72@gmail.com>
2025-12-14 20:16:49 +0100

    refactoring microservice for k8s: disable eureka

--- Customer/src/main/java/com/customer/Main.java
+++ Customer/src/main/java/com/customer/Main.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {
@@ -16,6 +18,11 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
         basePackages = "com.clients"
 
 )
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+}
+)
+
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
